39aa0a29e24770f856ab760f4cc7d8e3758fb73a
ablatieyv <abilailat@gmail.com>
2025-03-30 22:38:46 +0500

    feat: add problem3

---
+++ src/assignment1/Problem3.java
@@ -0,0 +1,20 @@
+package assignment1;
+
+public class Problem3 {
+    /**
+     * Recursively checks if a number is prime.
+     * Time Complexity: O(sqrt(n)) - We only check divisors up to sqrt(n).
+     */
+    public static boolean isPrime(int n, int divisor) {
+        if (n < 2) return false; // 0 and 1 are not prime
+        if (divisor * divisor > n) return true; // If no divisor found, it's prime
+        if (n % divisor == 0) return false; // If divisible, it's not prime
+        return isPrime(n, divisor + 1); // Check next divisor
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(7, 2));
+        System.out.println(isPrime(10, 2));
+    }
+}
+
