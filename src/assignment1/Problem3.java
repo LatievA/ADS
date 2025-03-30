facc65d98cfcbc400dc8fac7ba3f4ac9b3318f78
ablatieyv <abilailat@gmail.com>
2025-03-30 22:50:56 +0500

    fix: add message Prime or Composite instead of true or false

--- src/assignment1/Problem3.java
+++ src/assignment1/Problem3.java
@@ -12,9 +12,16 @@ public class Problem3 {
         return isPrime(n, divisor + 1); // Check next divisor
     }
 
+    public static String message(boolean isp){
+        if (isp) {
+            return "Prime";
+        }
+        return  "Composite";
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPrime(7, 2));
-        System.out.println(isPrime(10, 2));
+        System.out.println(message(isPrime(7, 2)));
+        System.out.println(message(isPrime(10, 2)));
     }
 }
 
