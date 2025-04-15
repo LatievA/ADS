37112d0fec04ad074c325eb92f8ace042f06bfaa
ablatieyv <abilailat@gmail.com>
2025-04-15 17:25:45 +0500

    feat: add MyMinHeap class

---
+++ src/assignment2/MyMinHeap.java
@@ -0,0 +1,70 @@
+package assignment2;
+
+public class MyMinHeap {
+    private MyArrayList<Integer> heap;
+
+    public MyMinHeap() {
+        heap = new MyArrayList<>();
+    }
+
+    public void add(int value) {
+        heap.add(value);
+        heapifyUp(heap.size() - 1);
+    }
+
+    public int poll() {
+        if (heap.size() == 0) throw new RuntimeException("Heap is empty");
+        int min = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        heapifyDown(0);
+        return min;
+    }
+
+    public int peek() {
+        if (heap.size() == 0) throw new RuntimeException("Heap is empty");
+        return heap.get(0);
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap.get(index) < heap.get(parent)) {
+                swap(index, parent);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void heapifyDown(int index) {
+        int size = heap.size();
+        while (index < size) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+
+            if (left < size && heap.get(left) < heap.get(smallest)) {
+                smallest = left;
+            }
+            if (right < size && heap.get(right) < heap.get(smallest)) {
+                smallest = right;
+            }
+            if (smallest == index) break;
+            swap(index, smallest);
+            index = smallest;
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+}
+
