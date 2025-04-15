882ae4ffdb46d7a2b21f451d33c08af8080513c2
ablatieyv <abilailat@gmail.com>
2025-04-15 17:24:26 +0500

    feat: add MyQueue class

---
+++ src/assignment2/MyQueue.java
@@ -0,0 +1,34 @@
+package assignment2;
+
+public class MyQueue<T> {
+    private MyList<T> list;
+
+    public MyQueue() {
+        list = new MyLinkedList<>();
+    }
+
+    public void enqueue(T element) {
+        list.add(element);
+    }
+
+    public T dequeue() {
+        if (list.size() == 0) throw new RuntimeException("Queue is empty");
+        T element = list.get(0);
+        list.remove(0);
+        return element;
+    }
+
+    public T peek() {
+        if (list.size() == 0) throw new RuntimeException("Queue is empty");
+        return list.get(0);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+}
+
